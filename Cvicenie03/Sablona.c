73107dcd68999cbdf6304b5b1e52818923312288
Katarína Olejková <71716586+kattie-left-the-chat@users.noreply.github.com>
2024-10-10 18:06:20 +0200

    Add files via upload

---
+++ Cvicenie03/Sablona.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+
+
+void JeKladne(int n)
+{
+
+}
+
+void SudeNeboLiche(int n)
+{
+
+}
+
+int Signum(int n)
+{
+
+}
+
+int Sucet(int n)
+{
+
+}
+
+void Nasobilka(int n)
+{
+
+}
+
+int SucetPole(int A[], int n)
+{
+	
+}
+
+float AritmetickyPrumer(int A[], int n)
+{
+
+}
+
+
+int main()
+{
+	//----------Priklady---------
+	// KED NAPROGRAMUJETE FUNKCIU A CHCETE JU VYSKUSAT ODKOMENTUJTE RIADKY (VYMAZTE "//" alebo "/* */") POD DANYM PRIKLADOM
+	// PRIKLAD[CISLO] ALE NECHAJTE ZAKOMENTOVANE
+	
+	// Priklad 1
+	//JeKladne(20);		
+
+	// Priklad 2
+	//SudeNeboLiche(13);	
+
+	// Priklad 3
+	//int sign = Signum(-5);
+	//printf("vysledok Signum: %d \n", sign);	
+
+	// Priklad 4
+	//int sucet = Sucet(6);
+	//printf("vysledok Sucet: %d \n", sucet);	
+
+	// Priklad 5
+	//Nasobilka(5);
+
+	// Priklad 6
+	/*
+	int pole1[] = { 2, 4, 6, 8, 10, 12 };
+	int velkost1 = 6;
+
+	int sucet_p = SucetPole(pole1, velkost1);
+	printf("vysledok SucetPole: %d \n", sucet_p);
+	*/
+
+	// Priklad 7
+	/*
+	int pole2[] = { 1, 5, 3, 7, 9, 4, 8, 2};
+	int velkost2 = 8;
+
+	float aritmeticky = AritmetickyPrumer(pole2, velkost2);
+	printf("vysledok AritmetickyPrumer: %f \n", aritmeticky);
+	*/
+}
---
+++ Cvicenie03/Sablona.py
@@ -0,0 +1,57 @@
+# KED CHCETE DEFINOVAT FUNKCIU MUSITE NAJPRV ODKOMENTOVAT JEJ HLAVICKU (VYMAZTE "#" PRED NAZVOM FUNKCIE)
+
+#def JeKladne(n):
+
+
+#def SudeNeboLiche(n):
+
+
+#def Signum(n):
+
+
+#def Sucet(n):
+
+
+#def Nasobilka(n):
+
+
+#def SucetPole(A, n):
+    
+
+#def AritmetickyPrumer(A, n):
+
+
+#----------Priklady---------
+# KED NAPROGRAMUJETE FUNKCIU A CHCETE JU VYSKUSAT ODKOMENTUJTE RIADKY (VYMAZTE "#") POD DANYM PRIKLADOM
+# PRIKLAD[CISLO] ALE NECHAJTE ZAKOMENTOVANE
+
+# Priklad 1
+#JeKladne(20)		    
+
+# Priklad 2
+#SudeNeboLiche(13)	    
+
+# Priklad 3
+#sign = Signum(-5)
+#print("vysledok Signum:", sign)	
+
+# Priklad 4
+#sucet = Sucet(6)
+#print("vysledok Sucet:", sucet) 
+
+# Priklad 5
+#Nasobilka(5)
+
+# Priklad 6
+#pole1 = [2, 4, 6, 8, 10, 12]
+#velkost1 = 6
+
+#sucet_p = SucetPole(pole1, velkost1)
+#print("vysledok SucetPole:", sucet_p)   
+
+# Priklad 7
+#pole2 = [1, 5, 3, 7, 9, 4, 8, 2]
+#velkost2 = 8
+
+#aritmeticky = AritmetickyPrumer(pole2, velkost2)
+#print("vysledok AritmetickyPrumer:", aritmeticky)
