17b634711151faef0932e6cda7e452de9b6643b2
Katarína Olejková <71716586+kattie-left-the-chat@users.noreply.github.com>
2024-11-05 16:06:08 +0100

    Add files via upload

---
+++ Cvicenie07/QuickSort-Sablona.c
@@ -0,0 +1,57 @@
+//sablona pro programovani tridicich algoritmu v C
+//ALGO1 2024/2025
+
+// pouzivame zakladni knihovny 
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+//nastavte si, jak velke chcete pole (kolik cisel budete tridit)
+#define VELIKOST 10             //TADY UPRAVIT
+//nastavte si rozsah cisel ke trideni
+#define CISLA_OD 0             //TADY UPRAVIT
+#define CISLA_DO 1000          //TADY UPRAVIT
+
+//nahodne naplni pole nachystane pro VELIKOST prvku hodnotami v zadanem rozsahu
+void napln_pole(int pole[]) {
+    time_t t;
+    srand((unsigned)time(&t));
+    for (int i = 0; i < VELIKOST; i++) {
+        pole[i] = CISLA_OD + rand() % CISLA_DO;
+    }
+}
+
+//vypise pole obshaujici VELIKOST prvku
+void vypis_pole(char* sorted, int pole[]) {
+    printf("%s \n", sorted);
+    for (int i = 0; i < VELIKOST; i++) {
+        printf("%d, ", pole[i]);
+    }
+
+    printf("\n");
+}
+
+//funkce provadejici trideni pomoci algoritmu quick sort
+void quick_sort(int pole[], p, r) {
+    printf("\nQUICK SORT: Zatim nic nedelam\n");  // TADY UPRAVIT (implementujte quick sort. Delka pole je v konstante VELIKOST.)
+}
+
+int main(void) {
+    //vytvorime, naplnime a vypiseme pole
+    int pole[VELIKOST];
+    napln_pole(pole);
+    vypis_pole("nesetrizene:", pole);
+
+    //---> zde provadejte sve pokusy s polem
+
+    quick_sort(pole, p, r);
+
+
+    //<--- dale kod nemente
+
+    //vypiseme pole po vykonani uprav
+    printf("\n");
+    vypis_pole("setrizene:", pole);
+    getchar(); //pockame na "enter" a skoncime
+    return 0;
+}
---
+++ Cvicenie07/QuickSort-Sablona.py
@@ -0,0 +1,44 @@
+#sablona pro programovani tridicich algoritmu v Pythonu
+#ALGO1 2024/2025
+
+# pouzivame knihovnu pro nahodne generovani cisel
+from random import randint
+
+
+# vytvori pole zadane velikosti s nahodnymi hodnotami v rozsahu dolni_limit az horni_limit         
+def vytvor_nahodne_pole(velikost, dolni_limit, horni_limit):
+    pole = []
+    while velikost > 0:
+        pole.append(randint(dolni_limit, horni_limit))
+        velikost = velikost - 1
+    return pole
+
+
+
+#nastavte si, jak velke chcete pole (kolik cisle budete tridit)
+VELIKOST = 10             #TADY UPRAVIT
+
+#nastavte si rozsah cisel ke trideni
+CISLA_OD = 0              #TADY UPRAVIT
+CISLA_DO = 1000           #TADY UPRAVIT
+
+
+
+# vytvori a vypise nahodne pole se zadanymi parametry
+moje_pole = vytvor_nahodne_pole(VELIKOST, CISLA_OD, CISLA_DO)
+print("Nesetrizene vstupni pole s", VELIKOST,"prvky:", moje_pole)
+
+
+#ODSUD DALE PISTE SVUJ KOD
+
+#funkce provadejici trideni pomoci algoritmu quick sort
+def quick_sort(pole, p, r):
+    print("QUICK SORT: Zatim nic nedelam")
+    # TADY UPRAVIT (implementujte quick sort. Delka pole je v konstante VELIKOST.)
+
+        
+#zavolani funkce na pripravene pole
+quick_sort(moje_pole, p, r)
+
+#vypis pole
+print("Setridene pole:", moje_pole)
